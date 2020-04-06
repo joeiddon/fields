bd8be928ea0440a11e2760e6e87f9503d142a30a
joeiddon <joedominiciddon@gmail.com>
2020-04-06 22:09:39 +0100

    u

---
+++ electric_potential/normal_lines/script.js
@@ -0,0 +1,377 @@
+'use strict';
+
+/*
+ * Good fundamental resource: https://webglfundamentals.org/
+ * Shaders are defined as strings in the `shaders.js` script.
+ *
+ * Ultimately WebGL is a 2d rasterization (fills pixels from vector graphic)
+ * library, but the Graphics Library Shader Language (GLSL) has features
+ * that make writing 3d engines easier. This includes things like matrix
+ * operations, dot products, and options like CULL_FACE and DEPTH (Z) BUFFER.
+ *
+ * For good, clear definitions of buffers etc. see:
+ * https://webglfundamentals.org/webgl/lessons/webgl-fundamentals.html
+ */
+
+/*
+ * TODO:
+ * add contour lines / better color schems
+ * draw lines (in 3d) of the equipotentials (contours) - try and work out how
+ * write a library to deal with vectors
+ * code to map mouse pointer to surface properly
+ * investigate if worth while to change from gl.STATIC
+ */
+
+// ensure this matches the vertex shader #define
+const MAX_CHARGES = 50;
+
+let canvas = document.getElementById('canvas');
+let gl = canvas.getContext('webgl');
+if (!gl) canvas.innerHTML = 'Oh no! WebGL is not supported.';
+
+function fit_canvas_to_screen(){
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+    gl.viewport(0, 0, canvas.width, canvas.height);
+}
+fit_canvas_to_screen();
+window.addEventListener('resize', fit_canvas_to_screen);
+
+let potential_program = misc.create_gl_program(potential_vertex_shader_src, fragment_shader_src);
+let charge_program = misc.create_gl_program(charge_vertex_shader_src, fragment_shader_src);
+let line_program = misc.create_gl_program(line_vertex_shader_src, fragment_shader_src);
+
+//set the color we want to clear to
+gl.clearColor(0, 0, 0, 1);
+
+let a_position_loc = gl.getAttribLocation(potential_program, 'a_position');
+let u_world_matrix_loc = gl.getUniformLocation(potential_program, 'u_world_matrix');
+let u_view_matrix_loc = gl.getUniformLocation(potential_program, 'u_view_matrix');
+let u_charges_loc = gl.getUniformLocation(potential_program, 'u_charges');
+let u_light_loc = gl.getUniformLocation(potential_program, 'u_light');
+
+let a_charge_position_loc = gl.getAttribLocation(charge_program, 'a_position');
+let a_charge_normal_loc = gl.getAttribLocation(charge_program, 'a_normal'); // spelling error ?
+let u_charge_world_matrix_loc = gl.getUniformLocation(charge_program, 'u_world_matrix');
+let u_charge_view_matrix_loc = gl.getUniformLocation(charge_program, 'u_view_matrix');
+let u_charge_light_loc = gl.getUniformLocation(charge_program, 'u_light');
+let u_charge_ball_translate_loc = gl.getUniformLocation(charge_program, 'u_ball_translate');
+
+let a_line_position_loc = gl.getAttribLocation(line_program, 'a_position');
+let u_line_world_matrix_loc = gl.getUniformLocation(line_program, 'u_world_matrix');
+let u_line_charges_loc = gl.getUniformLocation(line_program, 'u_charges');
+
+// if any locations are -1, that means they are not being used in the shaders,
+// so compiler got rid of them
+
+gl.enableVertexAttribArray(a_position_loc);
+gl.enableVertexAttribArray(a_charge_position_loc);
+gl.enableVertexAttribArray(a_charge_normal_loc);
+gl.enableVertexAttribArray(a_line_position_loc);
+
+let divisions = 200;
+let step = 2 / divisions;
+
+// positions are in charge / grid coordiantes x, y in [-1,1]
+// using a sampling method by working up in ints, then dividing to floats
+let positions = [];
+for (let xx = 0; xx <= divisions; xx ++) {
+    for (let yy = 0; yy <= divisions; yy ++) {
+        // conver the integer xx and yy into the appropriate floting ranges
+        let x = xx / divisions * 2 - 1;
+        let y = yy / divisions * 2 - 1;
+        positions.push(
+                 x, y,
+            x+step, y,
+            x+step, y+step,
+                 x, y,
+            x+step, y+step,
+                 x, y+step
+        );
+    }
+}
+
+let positions_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positions_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+let ball = [];
+let normals = [];
+
+let angle_step = 2 * Math.PI / 32;
+let radius = 0.02;
+
+function get_position_on_ball(yaw, pitch) {
+    return [
+        radius * Math.cos(pitch) * Math.sin(yaw),
+        radius * Math.sin(pitch),
+        radius * Math.cos(pitch) * Math.cos(yaw)
+    ];
+};
+
+
+for (let yaw = 0; yaw < Math.PI * 2; yaw += angle_step) {
+    for (let pitch = -Math.PI / 2; pitch < Math.PI / 2; pitch += angle_step) {
+        ball.push(...get_position_on_ball(yaw, pitch));
+        ball.push(...get_position_on_ball(yaw + angle_step, pitch));
+        ball.push(...get_position_on_ball(yaw + angle_step, pitch + angle_step));
+        ball.push(...get_position_on_ball(yaw, pitch));
+        ball.push(...get_position_on_ball(yaw, pitch + angle_step));
+        ball.push(...get_position_on_ball(yaw + angle_step, pitch + angle_step));
+        //for (let i = 0; i < 6; i++)
+        //normals.push(...get_position_on_ball(yaw + angle_step / 2 , pitch + angle_step / 2));
+        // pushing normals for actual corners, on a per-vertex basis!
+        normals.push(...(ball.slice(-6 * 3)));
+    }
+}
+
+let charge_positions_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, charge_positions_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(ball), gl.STATIC_DRAW);
+
+let charge_normals_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, charge_normals_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+
+//clockwise triangles are back-facing, counter-clockwise are front-facing
+//switch two verticies to easily flip direction a triangle is facing
+//"cull face" feature means kill (don't render) back-facing triangles
+//gl.enable(gl.CULL_FACE);
+
+//enable the z-buffer (only drawn if z component LESS than that already there)
+gl.enable(gl.DEPTH_TEST);
+
+function perspective_mat(fov, aspect, near, far){
+    return [
+        [ 1/(aspect*Math.tan(fov/2)),                 0,                     0,                     0],
+        [                          0, 1/Math.tan(fov/2),                     0,                     0],
+        [                          0,                 0, (far+near)/(far-near), 2*near*far/(near-far)],
+        [                          0,                 0,                     1,                     0]
+    ];
+}
+
+let fov = misc.deg_to_rad(50);
+let near = 0.1; //closest z-coordinate to be rendered
+let far = 50; //furthest z-coordianted to be rendered
+let m_perspective;
+
+function calculate_perspective_matrix() {
+    // put in function so can call again on canvas re-size when aspect changes
+    let aspect = canvas.width/canvas.height;
+    m_perspective = perspective_mat(fov, aspect, near, far);
+}
+calculate_perspective_matrix();
+window.addEventListener('resize', calculate_perspective_matrix);
+
+let cam = [0, 1.2, -3]; // issues when cam is up x-axis with panning of space_pitch !!
+
+// space is the grid
+let space_yaw = 0;
+let space_pitch = 0;
+
+let light = [-0.1, -1.5, 0.8]; // normalised in vertex shader
+
+// the "mouse charge" is always the first charge
+let charges = [
+    {position: [0, 0], magnitude: -0.5}
+];
+
+let matrices = {};
+
+function calculate_matrices(){
+    // matrices in right-to-left order (i.e. in order of application)
+
+    // rotates space according to space_yaw and space_pitch
+    matrices.rot = m4.multiply(m4.rotation_x(space_pitch), m4.rotation_y(space_yaw));
+    //transforms in front of cam's view
+    matrices.view = m4.multiply(m4.inverse(m4.orient(cam, [0,0,0])), matrices.rot);
+    //maps 3d to 2d
+    matrices.world = m4.multiply(m_perspective, matrices.view);
+}
+
+let test_charge = {
+    magnitude: 0.5,
+    position: [0, 0],
+    velocity: [0, 0]
+}
+
+let paused = false;
+let sticked = false;
+// press `p` key to pause, 's' to stick mouse charge
+document.addEventListener('keydown', e=>{
+    if (e.key == 'p') paused = !paused;
+    if (e.key == 's') sticked = !sticked;
+});
+
+function update_test_charge() {
+    if (paused) return;
+    /// !!!! // separate computation of electric field, E, to separate function !!!
+    let E = [0, 0];
+    let k = 0.00005;
+    for (let charge of charges) {
+        let d = ((charge.position[0]-test_charge.position[0])**2 + (charge.position[1]-test_charge.position[1])**2)**0.5;
+        if (d == 0) d = 0.001;
+        //if (d > 0.1) { // dont do anything if too close (note: if remove this, watch out for divide by zero!)
+            E[0] += k * charge.magnitude * (charge.position[0] - test_charge.position[0]) / d ** 2;
+            E[1] += k * charge.magnitude * (charge.position[1] - test_charge.position[1])  / d ** 2;
+        //}
+    }
+    let a = [
+        test_charge.magnitude * E[0],
+        test_charge.magnitude * E[1]
+    ];
+    // NOT INCLUDING TIME, t, IN THESE EQUATIONS, for now
+    // using s = ut + 1/2 at ^ 2
+    test_charge.position[0] += test_charge.velocity[0] + 0.5 * a[0];
+    test_charge.position[1] += test_charge.velocity[1] + 0.5 * a[1];
+    if (test_charge.position[0] > 1) {test_charge.position[0] = 1; test_charge.velocity = [0,0];}
+    if (test_charge.position[0] < -1) {test_charge.position[0] = -1; test_charge.velocity = [0,0];}
+    if (test_charge.position[1] > 1) {test_charge.position[1] = 1; test_charge.velocity = [0,0];}
+    if (test_charge.position[1] < -1) {test_charge.position[1] = -1; test_charge.velocity = [0,0];}
+    // using v = u + at
+    test_charge.velocity[0] += a[0];
+    test_charge.velocity[1] += a[1];
+}
+
+function compute_V(x, y) {
+    let V_SCALING_FACTOR = 0.04; // this should be extracted from the shader source code maybe!
+    let V = 0;
+    for (let charge of charges) {
+        let d = ((x - charge.position[0]) ** 2 + (y - charge.position[1]) ** 2) ** 0.5;
+        if (d == 0) d = 0.00001;
+        V += -1.0 * V_SCALING_FACTOR * charge.magnitude / d;
+    }
+    return V;
+}
+
+/* not used for making ball sit on surface as not true representation of position.
+function compute_normal(x, y) {
+    let dfdx = 0;
+    let dfdy = 0;
+    for (let charge of charges){
+        let d = ((x - charge.position[0]) ** 2 + (y - charge.position[1]) ** 2) ** 0.5;
+        if (d == 0) d = 0.0001;
+        dfdx += charge.magnitude * (x - charge.position[0]) / (d ** 3);
+        dfdy += charge.magnitude * (y - charge.position[1]) / (d ** 3);
+    }
+    let n = [-dfdx, 1, -dfdy];
+    return n.map(c => c / (n[0] ** 2 + n[1] ** 2 + n[2] ** 2) ** 0.5);
+}
+*/
+
+function calculate_test_charge_trans() {
+    let surface_position = [
+        test_charge.position[0],
+        compute_V(...test_charge.position) + radius,
+        test_charge.position[1]
+    ];
+    return surface_position;
+    /*
+    surface_position = [0, 1, 0];
+    let normal_offset = compute_normal(...test_charge.position);
+    normal_offset = [0, 1, 0];
+    return surface_position.map((c,i) => c + normal_offset[i] * radius);
+    */
+}
+
+
+let lines = [];
+let line_divisions = 40;
+for (let x = 0; x <= line_divisions; x ++) {
+    for (let y = 0; y <= line_divisions; y ++) {
+        let point = [x / line_divisions * 2 - 1, y / line_divisions * 2 - 1];
+        lines.push(...point, 0); //push tail
+        lines.push(...point, 1); //push head
+    }
+}
+let line_vector_bases_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, line_vector_bases_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(lines), gl.STATIC_DRAW);
+
+
+let last_time;
+let time_delta;
+function update(time) {
+    time_delta = time - (last_time || time);
+    last_time = time;
+    calculate_matrices();
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // draw electric potential field
+    gl.useProgram(potential_program);
+    gl.uniformMatrix4fv(u_view_matrix_loc, false, m4.gl_format(matrices.rot));
+    gl.uniformMatrix4fv(u_world_matrix_loc, false, m4.gl_format(matrices.world));
+    gl.uniform3fv(u_light_loc, new Float32Array(light));
+    let u_charges_data = [];
+    for (let i = 0; i < MAX_CHARGES; i ++){ // -1 because one taken up by mouse
+        if (i < charges.length) u_charges_data.push(...charges[i].position, charges[i].magnitude);
+        else u_charges_data.push(0, 0, 0);
+    }
+    gl.uniform3fv(u_charges_loc, new Float32Array(u_charges_data));
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positions_buffer);
+    gl.vertexAttribPointer(a_position_loc, 2, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, positions.length / 2);
+
+    // draw test charge
+    update_test_charge();
+    gl.useProgram(charge_program);
+    gl.uniform3fv(u_charge_ball_translate_loc, new Float32Array(calculate_test_charge_trans()));
+    gl.uniformMatrix4fv(u_charge_view_matrix_loc, false, m4.gl_format(matrices.rot));
+    gl.uniformMatrix4fv(u_charge_world_matrix_loc, false, m4.gl_format(matrices.world));
+    gl.uniform3fv(u_charge_light_loc, new Float32Array(light));
+    gl.bindBuffer(gl.ARRAY_BUFFER, charge_positions_buffer);
+    gl.vertexAttribPointer(a_charge_position_loc, 3, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, charge_normals_buffer);
+    gl.vertexAttribPointer(a_charge_normal_loc, 3, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, ball.length / 3);
+
+    gl.useProgram(line_program);
+    gl.uniform3fv(u_line_charges_loc, new Float32Array(u_charges_data));
+    gl.uniformMatrix4fv(u_line_world_matrix_loc, false, m4.gl_format(matrices.world));
+    gl.bindBuffer(gl.ARRAY_BUFFER, line_vector_bases_buffer);
+    gl.vertexAttribPointer(a_line_position_loc, 3, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.LINES, 0, lines.length / 3);
+
+    requestAnimationFrame(update);
+    update_info();
+}
+
+update();
+
+function toclipspace(x, y) {
+    return [
+        (x / canvas.width) * 2 - 1,
+        -((y / canvas.height) * 2 - 1),
+    ];
+}
+
+canvas.addEventListener('mousemove', function(e) {
+    let sensitivity = 400;
+    // if right click held down, so panning
+    if (e.buttons & 2) {
+        space_yaw -= e.movementX / sensitivity;
+        space_pitch -= e.movementY / sensitivity;
+    } else {
+        if (!sticked)
+        // move mouse charge
+        charges[0].position = toclipspace(e.x, e.y);
+    }
+});
+
+canvas.addEventListener('wheel', e => {charges[0].magnitude += e.deltaY / 200});
+canvas.addEventListener('click', e => {charges.push({position: [...charges[0].position], magnitude: charges[0].magnitude})}); // unpacked so creates new object
+
+function update_info(){
+    let join = (character) => (a,b) => a + character + b;
+    let format = (array, prec) => array.map(x => x.toFixed(prec || 2));
+    document.getElementById('info').innerText = [
+        ['fps', parseInt(1 / time_delta * 1e3)],
+        ['test charge x,y', format(test_charge.position)],
+        ['test charge potential (z)', compute_V(...test_charge.position).toFixed(2)],
+        ['test charge vx,vy', format(test_charge.velocity.map(c=>c*100))],
+        ['mouse charge', charges[0].position.map(c=>c.toFixed(3))]
+
+    ].map(a => a.reduce(join(' = '))).reduce(join('\n'));
+}
---
+++ electric_potential/normal_lines/shaders.js
@@ -0,0 +1,174 @@
+'use strict';
+
+/*
+ * Language is called OpenGL ES Shader Language or GLSL
+ * for short.
+ * See: https://www.khronos.org/files/webgl/webgl-reference-card-1_0.pdf
+ * and: https://www.khronos.org/files/opengles_shading_language.pdf
+ *
+ * http://learnwebgl.brown37.net/12_shader_language/glsl_control_structures.html
+ */
+
+let potential_vertex_shader_src = `
+//identifier prefixes like a_ and u_ signify types
+
+#define MAX_CHARGES 50
+#define V_SCALING_FACTOR 0.04
+#define V_MAX 0.8
+#define OSCILL 3.0
+#define PI 3.14159265358979
+
+// consider using structs ?
+
+attribute vec2 a_position;
+
+uniform mat4 u_world_matrix;
+uniform mat4 u_view_matrix;
+
+uniform vec3 u_light;
+
+// would probably better to pass a uniform of the number of charges used
+uniform vec3 u_charges[MAX_CHARGES]; // the z is the magnitude of the charge
+
+varying vec4 color;
+
+float compute_V() {
+    float V = 0.0;
+    for (int i = 0; i < MAX_CHARGES; i++){
+        // skip charges that are not being used
+        if (u_charges[i].z == 0.0) continue;
+        vec2 r = u_charges[i].xy - a_position.xy;
+        float Vi = -1.0 * V_SCALING_FACTOR * u_charges[i].z / length(r);
+        V += Vi;
+    }
+    // if (V > V_MAX) V = V_MAX;
+    return V;
+}
+
+vec3 compute_normal() {
+    float dfdx = 0.0;
+    float dfdy = 0.0;
+    for (int i = 0; i < MAX_CHARGES; i++){
+        // skip charges that are not being used
+        if (u_charges[i].z == 0.0) continue;
+        vec2 r = u_charges[i].xy - a_position.xy;
+        dfdx += u_charges[i].z * (a_position.x - u_charges[i].x) / pow(length(r), 3.0);
+        dfdy += u_charges[i].z * (a_position.y - u_charges[i].y) / pow(length(r), 3.0);
+    }
+    vec3 n = vec3(-dfdx, 1, -dfdy);
+    return n / length(n); // or use normalize(n)
+}
+
+void main(){
+    float V = compute_V();
+
+    vec3 vertex = vec3(a_position.x, V, a_position.y);
+    gl_Position = u_world_matrix * vec4(vertex, 1);
+
+    vec3 reflected_ray = -normalize(u_light);
+    vec3 n = compute_normal();
+    n = (u_view_matrix * vec4(n, 1)).xyz;
+    float intensity = dot(reflected_ray, n);
+    if (intensity < 0.0) intensity = 0.0;
+    //intensity = 0.8;
+    //color = vec4(1, 0, 0, 1);
+    float v = V;
+    color = vec4(
+        sin(v * OSCILL),
+        sin(v * OSCILL + PI * 2.0 / 3.0),
+        sin(v * OSCILL + PI * 4.0 / 3.0),
+        1
+    );
+    //color = vec4(1, 0.8-0.5*v, 0, 1);
+    color.xyz *= (0.6 + 0.4 * intensity);
+    //add bands of color for contours?
+    //if (float(int(V/10.0)) - V/10.0 < 0.05) color.xyz = vec3(1.0, 0, 0);
+}
+`;
+
+let charge_vertex_shader_src = `
+
+attribute vec3 a_position;
+attribute vec3 a_normal;
+
+uniform mat4 u_view_matrix;
+uniform mat4 u_world_matrix;
+
+uniform vec3 u_light;
+
+uniform vec3 u_ball_translate;
+
+// could conisder passing normal as varying then doing dot prod in frag shader
+varying vec4 color;
+
+void main(){
+    vec3 p = a_position + u_ball_translate;
+    gl_Position = u_world_matrix * vec4(p, 1);
+    vec3 n = normalize((u_view_matrix * vec4(a_normal, 1)).xyz);
+    vec3 reflected_ray = -normalize(u_light);
+    float intensity = dot(reflected_ray, n);
+    color = vec4(1, 0.2, 0.8, 1);
+    color.xyz *= (0.5 + 0.5 * intensity);
+}
+`
+
+let line_vertex_shader_src = `
+#define MAX_CHARGES 50
+#define V_SCALING_FACTOR 0.04
+#define LINE_LENGTH 0.05
+
+attribute vec3 a_position; // x,y is vector base, z is if head of vector or not
+
+uniform mat4 u_world_matrix;
+
+varying vec4 color;
+
+uniform vec3 u_charges[MAX_CHARGES]; // the z is the magnitude of the charge
+
+float compute_V(vec2 pos) {
+    float V = 0.0;
+    for (int i = 0; i < MAX_CHARGES; i++){
+        // skip charges that are not being used
+        if (u_charges[i].z == 0.0) continue;
+        vec2 r = u_charges[i].xy - pos.xy;
+        float Vi = -1.0 * V_SCALING_FACTOR * u_charges[i].z / length(r);
+        V += Vi;
+    }
+    // if (V > V_MAX) V = V_MAX;
+    return V;
+}
+
+vec3 compute_normal(vec2 pos) {
+    float dfdx = 0.0;
+    float dfdy = 0.0;
+    for (int i = 0; i < MAX_CHARGES; i++){
+        // skip charges that are not being used
+        if (u_charges[i].z == 0.0) continue;
+        vec2 r = u_charges[i].xy - pos.xy;
+        dfdx += u_charges[i].z * (a_position.x - u_charges[i].x) / pow(length(r), 3.0);
+        dfdy += u_charges[i].z * (a_position.y - u_charges[i].y) / pow(length(r), 3.0);
+    }
+    vec3 n = vec3(-dfdx, 1, -dfdy);
+    return n / length(n); // or use normalize(n)
+}
+
+void main(){
+    vec3 p = vec3(a_position.x, compute_V(a_position.xy), a_position.y);
+    if (a_position.z == 0.0) {
+        p += vec3(compute_normal(a_position.xy)) * LINE_LENGTH;
+    }
+    gl_Position = u_world_matrix * vec4(p, 1);
+    color = vec4(1, 1, 1, 1);
+}
+`
+
+
+let fragment_shader_src = `
+precision mediump float;
+
+varying vec4 color;
+
+void main(){
+    gl_FragColor = color;
+}
+`;
